f2784fe6c21c9c5fde938b1a610b4e22923bac91
ISK <72429155+IS-MAIL-SK@users.noreply.github.com>
2025-08-01 14:53:50 +0530

    Delete Functions.php

--- Functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-// functions.php - Theme Functions
-require_once get_template_directory() . '/inc/theme-setup.php';
-require_once get_template_directory() . '/inc/enqueue.php';
-require_once get_template_directory() . '/inc/widgets.php';
-?>
